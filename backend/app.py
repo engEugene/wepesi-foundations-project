552dfee86c21c9b98062dfb593a6d3b64a8ea82c
Eugene Koech <e.koech@alustudent.com>
2025-10-25 18:27:38 +0300

    chore:setup user model

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///volunteer.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/models/users.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timezone
+from app import db, bcrypt
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(
+        db.Enum("volunteer", "organization", "admin", name="user_roles"),
+        nullable=False,
+        default="volunteer"
+    )
+    total_volunteer_hours = db.Column(db.Numeric(5,2), default=0)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(
+        db.DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc)
+    )
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f"<User {self.name} ({self.role})>"
