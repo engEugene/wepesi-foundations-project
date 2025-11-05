d6092f1d6d1ea59a1a4d1fdeb9518234cb38a0c4
Eugene Koech <e.koech@alustudent.com>
2025-11-05 10:47:09 +0300

    try enabling https in localhost - cors issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
-    }),tailwindcss(),
+    }),
+    tailwindcss(),
   ],
-})
+  server: {
+    https: {
+      key: fs.readFileSync("./localhost+2-key.pem"),
+      cert: fs.readFileSync("./localhost+2.pem"),
+    },
+    host:"localhost",
+    port: 5173,
+  },
+});
