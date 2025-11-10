5c75e3e542f567992f9f4745f3ecb45cbd377566
Abbieomol <l.omol@alustudent.com>
2025-11-10 21:06:48 +0300

    event and onboarding pages

---
+++ frontend/src/pages/OrganizationOnboardingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OrganizationOnboardingForm from "../components/OrganizationOnboardingForm";
+
+const OrganizationOnboardingPage = () => {
+  return (
+    <div className="page-container">
+      <h1>Organization Onboarding</h1>
+      <OrganizationOnboardingForm />
+    </div>
+  );
+};
+
+export default OrganizationOnboardingPage;
